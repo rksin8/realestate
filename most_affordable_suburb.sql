e6320ee27bba07d97138492ab15275e23af0c3f4
Ranjeet Singh <ranjeet.singh2@monash.edu>
2024-08-13 03:18:37 +1000

    most affordable suburbs

---
+++ most_affordable_suburb.sql
@@ -0,0 +1,57 @@
+/*
+1. Most small families would be looking for apartment with 40-60 m2 in size. 
+Identify the top 5 most affordable neighborhoods in warsaw.
+
+*/
+
+select * from realestat_flatten where rent is not null limit 20;
+
+select rent from realestat_flatten limit 20;
+
+
+-- Solution 1: 
+select suburb
+	, count(*) as cnt
+	from realestat_flatten where 
+	state in ('Vic')
+	and propertytype='apartment'
+	and rent is not null
+	and rent > 700
+	group by suburb
+	order by cnt desc limit 3;
+
+
+
+	
+-- Solution 2
+
+select suburb
+	, count(1)
+	, rank() over(order by suburb desc ) as rn
+	from realestat_flatten where 
+	state in ('Vic')
+	and propertytype='apartment'
+	and rent is not null
+	and rent > 1000
+	group by suburb
+
+	
+
+with cte as
+	(
+	select suburb, round(avg(rent),2) avg_price, count(1) as no_of_apartments
+	, rank() over(order by round(avg(rent),2) ) as rnk
+	from realestat_flatten where 
+	state in ('Vic')
+	and propertytype='apartment'
+	and rent is not null
+	and rent > 700
+	group by suburb
+	)
+	select *
+	from cte
+	where rnk <= 5;
+
+
+
+
