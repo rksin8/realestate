835069fc7c94bc0d15d997f7e879829d76b92609
Ranjeet Singh <ranjeet.singh2@monash.edu>
2024-08-12 18:18:06 +1000

    appartment by suburbs between price range

---
+++ apartement by suburbs between price_range.sql
@@ -0,0 +1,15 @@
+/*
+1. I want to buy an apartement which is around 90-100 m2 and within a range of 800000 to 1M,
+display the suburbs in Melbourne where I can find such appartments
+*/
+
+select * from realestat_flatten;
+
+
+select state
+	, round(avg(rent),2) as avg_rent 
+	from realestat_flatten where state='Vic'
+	and propertytype='apartment'
+	and rent between 700 and 800 --and surface_area between a and b
+	group by state
+	order by count(1) desc;
