d28c2489d93c8ceddb9de1c0f441f72dbe9a1950
Ranjeet Singh <ranjeet.singh2@monash.edu>
2024-08-29 10:52:26 +1000

    avg rent victoria by suburbs

---
+++ average_rent_for_2_3bedrooms_apartment.sql
@@ -0,0 +1,20 @@
+/*
+1. What is the avg sale price for apartments 2-3 bedrooms in major cities of Australia?
+
+*/
+
+select * from realestat_flatten where rent is not null limit 20;
+
+select rent from realestat_flatten limit 20;
+
+
+select state,
+	avg(rent) as avg_rent
+	from realestat_flatten where 
+	state in ('Vic','NSW','WA')
+	and propertytype='apartment'
+	and rent is not null and rent > 100
+	and bedrooms between 2 and 3
+	group by state
+	order by avg_rent desc;
+
---
+++ avg_rent_victoria_suburbs.sql
@@ -0,0 +1,21 @@
+/*
+1. What is the avg rent for apartments in Victoria in different suburbs? 
+Categories the result based on number of bedrooms?
+
+*/
+
+select * from realestat_flatten where rent is not null limit 20;
+
+select rent from realestat_flatten limit 20;
+
+
+select suburb
+	, bedrooms
+	, round(avg(rent),2) as avg_rent
+	from realestat_flatten where 
+	state in ('Vic')
+	and propertytype='apartment'
+	and rent is not null and rent > 100
+	group by suburb, bedrooms
+	order by avg_rent desc;
+
