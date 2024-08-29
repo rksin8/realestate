81afde2c20e6551640b460e998a023839cec8bdb
Ranjeet Singh <ranjeet.singh2@monash.edu>
2024-08-29 10:50:40 +1000

    clean up

--- part_1_flattening_data.sql
+++ part_1_flattening_data.sql
@@ -13,8 +13,31 @@ copy target  from 'C:/Users/Admin/Documents/Data Analyst Portfolio Projects/real
 
 select * from target;
 
+select data->'_links'->'prettyUrl'->>'href' from target;
 
 
+select data->'price' ->> 'display'  as rent
+	from target;
+
+select data->'price' ->> 'display'  as rent1
+	, substring(regexp_replace(data->'price' ->> 'display',',','' ),'([0-9.]+)')::numeric  as rent 
+	from target;
+
+SELECT REGEXP_REPLACE('John Doe','(.*) (.*)','\2, \1'); 
+
+select regexp_replace('123,56€',',','.' );
+	
+select  substring(regexp_replace('$1,380 per week',',','' ),'(\$[0-9.]+)');
+
+select  data-> 'address' as address
+	, data->> 'title' as title
+	from target limit 5;
+
+
+-- Check listing column
+select  data-> 'address' ->> 'suburb' as suburb
+	, data-> 'address' ->> 'state' as state
+	from target;
 -- Check listing column
 select  NULLIF(data->> 'listingId','')::int as listingId
 	from target limit 5;
@@ -23,7 +46,7 @@ select  NULLIF(data->> 'listingId','')::int as listingId
 select 
 	NULLIF(data->> 'listingId','')::int as listingId
 	, NULLIF(regexp_replace(data-> 'price' ->> 'display', '\D','','g'), '')::numeric as rent
-	from target limit 5;
+	from target;
 
 
 -- Flatten useful columns
@@ -42,13 +65,16 @@ select  NULLIF(data->> 'listingId','')::int as listingId
 
 
 
+
+drop table realestat_flatten;
+
 -- create a new table of flatten data
 create table if not exists realestat_flatten
 as 
 select  NULLIF(data->> 'listingId','')::int as listingId
 	, data ->> 'propertyType' as propertyType
 	, NULLIF(data->> 'isRentChannel','')::boolean  as channelType
-	, NULLIF(regexp_replace(data-> 'price' ->> 'display', '\D','','g'), '')::numeric as rent
+	, substring(regexp_replace(data->'price' ->> 'display',',','' ),'([0-9.]+)')::numeric as rent
 	, NULLIF(data-> 'features' -> 'general' ->>'bedrooms','')::int as bedrooms  
 	, NULLIF(data-> 'features' ->'general' ->> 'bathrooms' ,'')::int as bathrooms
 	, NULLIF(data-> 'features' -> 'general' ->>'parkingSpaces' ,'')::int as parkingSpaces
@@ -56,12 +82,23 @@ select  NULLIF(data->> 'listingId','')::int as listingId
 	, data-> 'address' ->> 'suburb' as suburb
 	, data-> 'address' ->> 'state' as state
 	, NULLIF(data-> 'address' ->>'postCode' ,'')::int as postCode
+	, data->> 'title' as title
+	, data->'_links'->'prettyUrl'->>'href' as url
 	from target;
+
+
+select * from realestat_flatten where 
+	suburb like 'Sydney' 
+	or suburb like 'Melbourne%'
+	or suburb like 'Brisbane%'
+	or suburb like 'Perth%'
+	or suburb like 'Adelaide%';
+
 
 -- re-check flatten data
 select count(1) from realestat_flatten;
 
-select * from realestat_flatten;
+select * from realestat_flatten limit 5;
 
 -- Delete temporary table
 COMMIT;
